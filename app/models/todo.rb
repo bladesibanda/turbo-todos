955bea8183c75222cdd013285568bd02fb087cd2
Blessed Sibanda <bladesibanda@gmail.com>
2023-02-13 00:05:07 +0200

    turbo powered todo app

---
+++ app/models/todo.rb
@@ -0,0 +1,8 @@
+class Todo < ApplicationRecord
+  after_update_commit { broadcast_append_to "todos" }
+  validates :title, presence: true
+  enum status: {
+    incomplete: 0,
+    complete: 1,
+  }
+end
